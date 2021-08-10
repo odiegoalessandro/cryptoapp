ca8cd7da98f25b07d7a21b19225b3d1e1b1d6eb8
Diego Alessandro Da Cruz Martins <diegoalessandrodacruzmartins@gmail.com>
2021-08-10 19:16:18 -0300

    create search bar

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,18 +1,17 @@
 import { Tr, Td } from "@chakra-ui/react";
-import Image from 'next/image'
 
 export default function Coin({ coin }){
   return(
     <Tr>
       <Td textAlign="center">{coin.market_cap_rank}</Td>
-      <Td textAlign="center"><Image width="30" height="30" src={coin.image} /></Td>
+      <Td textAlign="center"><img style={{width: "30px", height: "30px"}} src={coin.image} /></Td>
       <Td textAlign="center">{coin.symbol}</Td>
       <Td textAlign="center">{coin.name}</Td>
-      <Td textAlign="center">{Number(coin.current_price).toFixed(2)}</Td>
-      <Td textAlign="center">{Number(coin.high_24h).toFixed(2)}</Td>
-      <Td textAlign="center">{Number(coin.low_24h).toFixed(2)}</Td>
-      <Td textAlign="center">{Number(coin.ath).toFixed(2)}</Td>
-      <Td textAlign="center">{Number(coin.atl).toFixed(2)}</Td>
+      <Td textAlign="center">R${Number(coin.current_price).toFixed(2)}</Td>
+      <Td textAlign="center">R${Number(coin.high_24h).toFixed(2)}</Td>
+      <Td textAlign="center">R${Number(coin.low_24h).toFixed(2)}</Td>
+      <Td textAlign="center">R${Number(coin.ath).toFixed(2)}</Td>
+      <Td textAlign="center">R${Number(coin.atl).toFixed(2)}</Td>
     </Tr>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,36 @@
+import { Center, Input } from "@chakra-ui/react"
 import { GetStaticProps } from "next"
+import { useState } from "react"
 import CoinList from "../components/CoinList"
 import crypto from "../services/crypto"
 
 export default function Home({ cryptocoins }) {
+  const [search, setSearch] = useState('')
+  const handleChange = e => {
+    e.preventDefault()
+
+    setSearch(e.target.value.toLowerCase())
+  }
+  
+  const filtredCoins = cryptocoins.filter(coin => 
+    coin.name.toLowerCase().includes(search.trim()) 
+    || coin.symbol.toLowerCase().includes(search.trim())
+  )
+
   return (
-    <CoinList coins={cryptocoins} />
+    <>
+      <Center>
+        <Input 
+          w="20rem"
+          my="10"
+          value={search}
+          onChange={handleChange}
+          placeholder="Pesquise(nomes, simbolos)"  
+        />
+      </Center>
+
+      <CoinList coins={filtredCoins} />
+    </>
   )
 }
 
